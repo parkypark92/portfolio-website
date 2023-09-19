23ba1b9f4f32d8c2d901c9d8f92eab8c9bb4342a
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-09-19 18:01:53 +0100

    get basic responsive display for contact section. Make navigation buttons scroll to relevant section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./style.css";
+
+const projectsSection = document.querySelector(".projects-section");
+const aboutSection = document.querySelector(".about-section");
+const contactSection = document.querySelector(".contact-section");
+const aboutButton = document.querySelector(".about-button");
+const projectsButton = document.querySelector(".projects-button");
+const contactButton = document.querySelector(".contact-button");
+
+aboutButton.addEventListener("click", () => {
+  scrollTo(aboutSection);
+});
+
+projectsButton.addEventListener("click", () => {
+  scrollTo(projectsSection);
+});
+
+contactButton.addEventListener("click", () => {
+  scrollTo(contactSection);
+});
+
+function scrollTo(section) {
+  section.scrollIntoView({ behavior: "smooth" });
+}
