16aa9b6eaa6a73704e1f6e1d601fe598b1a9d351
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-09-21 19:32:46 +0100

    make nav bar initially invisible and have it appear once the intro message reaches the top of the screen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import "./style.css";
 
+const navBar = document.querySelector("nav");
+const introMessage = document.querySelector(".intro-message");
 const projectsSection = document.querySelector(".projects-section");
 const aboutSection = document.querySelector(".about-section");
 const contactSection = document.querySelector(".contact-section");
 const aboutButton = document.querySelector(".about-button");
 const projectsButton = document.querySelector(".projects-button");
 const contactButton = document.querySelector(".contact-button");
+
+function scrollTo(section) {
+  section.scrollIntoView({ behavior: "smooth" });
+}
 
 aboutButton.addEventListener("click", () => {
   scrollTo(aboutSection);
@@ -19,6 +25,15 @@ contactButton.addEventListener("click", () => {
   scrollTo(contactSection);
 });
 
-function scrollTo(section) {
-  section.scrollIntoView({ behavior: "smooth" });
+function changeNavOpacity() {
+  const messagePosition = introMessage.getBoundingClientRect().top;
+  if (messagePosition <= 0) {
+    navBar.classList.remove("invisible");
+    introMessage.classList.add("invisible");
+  } else {
+    navBar.classList.add("invisible");
+    introMessage.classList.remove("invisible");
+  }
 }
+
+window.addEventListener("scroll", changeNavOpacity);
