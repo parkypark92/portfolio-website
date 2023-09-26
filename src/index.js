34f8f6ff0edd1b0f865defbb283b737cce85e91b
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-09-26 11:10:57 +0100

    highlight section in nav bar when scrolled into view

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const introMessage = document.querySelector(".intro-message");
 const projectsSection = document.querySelector(".projects-section");
 const aboutSection = document.querySelector(".about-section");
 const contactSection = document.querySelector(".contact-section");
+const sections = [projectsSection, aboutSection, contactSection];
 const aboutButton = document.querySelector(".about-button");
 const projectsButton = document.querySelector(".projects-button");
 const contactButton = document.querySelector(".contact-button");
@@ -36,4 +37,26 @@ function changeNavOpacity() {
   }
 }
 
+function navHighlight() {
+  const scrollY = window.scrollY;
+  for (let current of sections) {
+    const sectionId = current.getAttribute("id");
+    const sectionPosition = current.offsetTop - 300;
+    const sectionHeight = current.offsetHeight;
+    if (
+      scrollY > sectionPosition &&
+      scrollY <= sectionPosition + sectionHeight
+    ) {
+      document
+        .querySelector(`.${sectionId}-button`)
+        .classList.add("highlighted");
+    } else {
+      document
+        .querySelector(`.${sectionId}-button`)
+        .classList.remove("highlighted");
+    }
+  }
+}
+
 window.addEventListener("scroll", changeNavOpacity);
+window.addEventListener("scroll", navHighlight);
